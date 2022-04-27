ded32cc31e0f42ff81936b07b6403c100e377e38
SebTota <kidokyo1@gmail.com>
2022-04-26 21:52:40 -0700

    Added logic for getting new strava event details

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from email.base64mime import body_decode
-from strava_calendar_summary_utils import Logging
+from strava_calendar_summary_utils import Logging, StravaUtil
+from strava_calendar_summary_data_access_layer import StravaEvent, User, UserController
 
 import base64
 import logging
@@ -9,8 +10,25 @@ def start(event, context):
 
     if 'data' in event:
         body = base64.b64decode(event['data']).decode('utf-8')
-        logging.info(body)
     else:
-        logging.error('Could not find data for function: {}'.format(context.event_id))
+        logging.error('Error launching event processing function. \
+            Could not find data for function: {}'.format(context.event_id))
 
-# pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    try:
+        strava_event: StravaEvent = StravaEvent.from_dict(body)
+    except Exception as e:
+        logging.error('Failed creating Strava Event from cloud function data. Error: ' + e)
+
+    logging.info('Event processing function starting to process \
+        event: {} for user: {}'.format(strava_event.object_id, strava_event.athlete_id))
+
+    try:
+        user: User = UserController().get_by_id(strava_event.athlete_id)
+    except Exception as e:
+        logging.error('Failed getting a user for \
+            event: {} for user: {}. Error: {}'.format(strava_event.id, strava_event.athlete_id, e))
+    
+    strava_util: StravaUtil = StravaUtil(user)
+    activity = strava_util.get_activity(strava_event.id)
+
+    logging.info('Adding new event to calendar')
